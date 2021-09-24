ae5c89670b018e6bd3c9f4542d17cb03589ceac1
Mattias Kleman <mattias.kleman@gmail.com>
2021-09-24 13:31:31 +0200

    hej

--- NumbersGame/Program.cs
+++ NumbersGame/Program.cs
@@ -7,10 +7,11 @@ namespace NumbersGame
         static void Main(string[] args)
         {
             Console.WriteLine("Välkommen! Jag tänker på ett nummer. Kan du gissa vilket? Du får fem försök");
+            Console.WriteLine("hejsan);
         }
-        
 
-	    
+
+       
 
     }
 }
