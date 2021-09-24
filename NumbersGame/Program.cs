87d4717761741839bd672ad22d7f92818b2147a3
Mattias <Mattias@DESKTOP-5JCLISP>
2021-09-24 13:28:08 +0200

    Add project files.

---
+++ NumbersGame/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace NumbersGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Välkommen! Jag tänker på ett nummer. Kan du gissa vilket? Du får fem försök");
+        }
+        
+
+	    
+
+    }
+}
