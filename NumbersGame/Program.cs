bba60bc212485e858b109ed4fec950c9265b93a6
Mattias Kleman <mattias.kleman@gmail.com>
2021-09-28 12:10:01 +0200

    Jobbar med min andra metod

--- NumbersGame/Program.cs
+++ NumbersGame/Program.cs
@@ -10,21 +10,22 @@ namespace NumbersGame
             int number = random.Next(1, 10);
             Console.WriteLine("Välkommen! Jag tänker på ett nummer. Kan du gissa vilket? Du får fem försök");
             int answer;
+            Program P = new Program();
+            int guess = P.CheckGuess;
 
 
-
-            for (int i = 0; i <5; i++)
+            for (int i = 0; i < 5; i++)
             {
                 answer = int.Parse(Console.ReadLine());
                 if (answer == number)
                 {
                     Console.WriteLine("Grattis du har svarat rätt! :)");
-                    
+
                 }
                 else if (answer < number)
                 {
                     Console.WriteLine("Tyvärr du gissade för lågt!");
-                    
+
                 }
                 else if (answer > number)
                 {
@@ -34,15 +35,22 @@ namespace NumbersGame
                 if (i >= answer)
                 {
                     Console.WriteLine("Tyvärr du har gissat fel för många gånger");
+                    
                 }
 
-                
+
             }
 
 
-          
+
         }
-        
+        static int CheckGuess(int guess)
+        {
+            Console.WriteLine("Du har gisssat"+ guess ++);
+        }
+
     }
-   
 }
+   
+
+
