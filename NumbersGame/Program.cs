3dbe127a7f0495ce2ddccaf3e6fa22f26af4028a
Mattias Kleman <mattias.kleman@gmail.com>
2021-09-28 10:35:12 +0200

    Har fixat ena delen utav labben

--- NumbersGame/Program.cs
+++ NumbersGame/Program.cs
@@ -6,12 +6,43 @@ namespace NumbersGame
     {
         static void Main(string[] args)
         {
+            Random random = new Random();
+            int number = random.Next(1, 10);
             Console.WriteLine("Välkommen! Jag tänker på ett nummer. Kan du gissa vilket? Du får fem försök");
-            Console.WriteLine("hejsan);
+            int answer;
+
+
+
+            for (int i = 0; i <5; i++)
+            {
+                answer = int.Parse(Console.ReadLine());
+                if (answer == number)
+                {
+                    Console.WriteLine("Grattis du har svarat rätt! :)");
+                    
+                }
+                else if (answer < number)
+                {
+                    Console.WriteLine("Tyvärr du gissade för lågt!");
+                    
+                }
+                else if (answer > number)
+                {
+                    Console.WriteLine("Tyvärr du gissade för högt!");
+                }
+
+                if (i >= answer)
+                {
+                    Console.WriteLine("Tyvärr du har gissat fel för många gånger");
+                }
+
+                
+            }
+
+
+          
         }
-
-
-       
-
+        
     }
+   
 }
